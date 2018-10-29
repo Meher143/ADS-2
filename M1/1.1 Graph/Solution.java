1cff9f046c440a603c3b2793560ef4e385bbf208
Meher143 <krishmeher@msitprogram.net>
2018-10-29 22:49:13 +0530

    "ads2_m1_a1_1: 7/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- M1/1.1 Graph/Solution.java
+++ M1/1.1 Graph/Solution.java
@@ -1,8 +1,49 @@
-interface Graph {
-    public int V();
-    public int E();
-    public void addEdge(int v, int w);
-    public Iterable<Integer> adj(int v);
-    public boolean hasEdge(int v, int w);
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+
+    }
+    /**
+     * Main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String type = sc.nextLine();
+        int nvertices = Integer.parseInt(sc.nextLine());
+        int nedges = Integer.parseInt(sc.nextLine());
+        String[] keys = sc.nextLine().split(",");
+        Newgraph g = new Newgraph(nvertices);
+        while (sc.hasNext()) {
+            String[] conn = sc.nextLine().split(" ");
+            g.addEdge(Integer.parseInt(conn[0]),
+                Integer.parseInt(conn[1]));
+        }
+        switch (type) {
+            case "List":
+            try {
+                g.display1(nvertices, nedges, keys);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            break;
+            case "Matrix":
+            try {
+                g.display2(nvertices, nedges);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+            break;
+            default:
+            break;
+        }
+    }
 }
 
