d461320aa7de9273cb27a7f9490b937bc1acea0c
Meher143 <krishmeher@msitprogram.net>
2018-10-31 22:40:32 +0530

    "ads2_m3_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- M3/3.1 Directed Cycle/Solution.java
+++ M3/3.1 Directed Cycle/Solution.java
@@ -1,19 +1,25 @@
 import java.util.Scanner;
-final class Solution{
+final class Solution {
 private Solution() {
 
 }
-public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int nvert = Integer.parseInt(sc.nextLine());
-		int nedge = Integer.parseInt(sc.nextLine());
-		Digraph d = new Digraph(nvert);
-		for (int i = 0; i < nedge; i++) {
-			String[] str = sc.nextLine().split(" ");
-			d.addEdge(Integer.parseInt(str[0]), Integer.parseInt(str[1]));
+/**
+ * { function_description }.
+ *
+ * @param      args  The arguments
+ */
+public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int nvert = Integer.parseInt(sc.nextLine());
+        int nedge = Integer.parseInt(sc.nextLine());
+        Digraph d = new Digraph(nvert);
+        for (int i = 0; i < nedge; i++) {
+            String[] str = sc.nextLine().split(" ");
+            d.addEdge(Integer.parseInt(str[0]), 
+                Integer.parseInt(str[1]));
 
-		}
-		DirectedCycle cyc = new DirectedCycle(d);
+        }
+        DirectedCycle cyc = new DirectedCycle(d);
         if (cyc.hasCycle()) {
             System.out.println("Cycle exists.");
         } else {
