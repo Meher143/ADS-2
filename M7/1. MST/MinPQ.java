255501b76c4d1f112bb7ab411f6190c719e2fd1f
Meher143 <krishmeher@msitprogram.net>
2018-11-06 00:37:57 +0530

    "ads2_m7_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- M7/1. MST/MinPQ.java
+++ M7/1. MST/MinPQ.java
@@ -1,316 +1,222 @@
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-
+import java.util.Comparator;
 /**
- *  The {@code IndexMinPQ} class represents an indexed priority queue of generic keys.
- *  It supports the usual <em>insert</em> and <em>delete-the-minimum</em>
- *  operations, along with <em>delete</em> and <em>change-the-key</em> 
- *  methods. In order to let the client refer to keys on the priority queue,
- *  an integer between {@code 0} and {@code maxN - 1}
- *  is associated with each key—the client uses this integer to specify
- *  which key to delete or change.
- *  It also supports methods for peeking at the minimum key,
- *  testing if the priority queue is empty, and iterating through
- *  the keys.
- *  <p>
- *  This implementation uses a binary heap along with an array to associate
- *  keys with integers in the given range.
- *  The <em>insert</em>, <em>delete-the-minimum</em>, <em>delete</em>,
- *  <em>change-key</em>, <em>decrease-key</em>, and <em>increase-key</em>
- *  operations take logarithmic time.
- *  The <em>is-empty</em>, <em>size</em>, <em>min-index</em>, <em>min-key</em>,
- *  and <em>key-of</em> operations take constant time.
- *  Construction takes time proportional to the specified capacity.
- *  <p>
- *  For additional documentation, see <a href="https://algs4.cs.princeton.edu/24pq">Section 2.4</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
- *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
- *
- *  @param <Key> the generic type of key on this priority queue
+ * Class for minimum pq.
+ * @param      <Key>  The key
  */
-class IndexMinPQ<Key extends Comparable<Key>> implements Iterable<Integer> {
-    private int maxN;        // maximum number of elements on PQ
-    private int n;           // number of elements on PQ
-    private int[] pq;        // binary heap using 1-based indexing
-    private int[] qp;        // inverse of pq - qp[pq[i]] = pq[qp[i]] = i
-    private Key[] keys;      // keys[i] = priority of i
-
+public class MinPQ<Key> {
     /**
-     * Initializes an empty indexed priority queue with indices between {@code 0}
-     * and {@code maxN - 1}.
-     * @param  maxN the keys on this priority queue are index from {@code 0}
-     *         {@code maxN - 1}
-     * @throws IllegalArgumentException if {@code maxN < 0}
+     * pq array of type Key.
      */
-    public IndexMinPQ(int maxN) {
-        if (maxN < 0) throw new IllegalArgumentException();
-        this.maxN = maxN;
+    private Key[] pq;
+    /**
+     * size of array pq.
+     */
+    private int n;
+    /**
+     * comparator of type Comparator.
+     */
+    private Comparator<Key> comparator;
+    /**
+     * Initializes an empty priority queue with the given initial capacity.
+     * @param  initCapacity the initial capacity of this priority queue
+     */
+    public MinPQ(final int initCapacity) {
+        pq = (Key[]) new Object[initCapacity + 1];
         n = 0;
-        keys = (Key[]) new Comparable[maxN + 1];    // make this of length maxN??
-        pq   = new int[maxN + 1];
-        qp   = new int[maxN + 1];                   // make this of length maxN??
-        for (int i = 0; i <= maxN; i++)
-            qp[i] = -1;
     }
-
+    /**
+     * Initializes an empty priority queue.
+     */
+    public MinPQ() {
+        this(1);
+    }
+    /**
+     * Initializes an empty priority queue with the given initial capacity,
+     * using the given comparator.
+     * @param  initCapacity the initial capacity of this priority queue
+     * @param  comparatorr the order in which to compare the keys
+     */
+    public MinPQ(final int initCapacity, final Comparator<Key> comparatorr) {
+        this.comparator = comparatorr;
+        pq = (Key[]) new Object[initCapacity + 1];
+        n = 0;
+    }
+    /**
+     * Initializes an empty priority queue using the given comparator.
+     * @param  comparatorr the order in which to compare the keys
+     */
+    public MinPQ(final Comparator<Key> comparatorr) {
+        this(1, comparatorr);
+    }
+    /**
+     * Initializes a priority queue from the array of keys.
+     * Takes time proportional to the number of keys,
+     * using sink-based heap construction.
+     * @param  keys the array of keys
+     */
+    public MinPQ(final Key[] keys) {
+        n = keys.length;
+        pq = (Key[]) new Object[keys.length + 1];
+        for (int i = 0; i < n; i++) {
+            pq[i + 1] = keys[i];
+        }
+        for (int k = n / 2; k >= 1; k--) {
+            sink(k);
+        }
+    }
     /**
      * Returns true if this priority queue is empty.
-     *
      * @return {@code true} if this priority queue is empty;
      *         {@code false} otherwise
+     * time complexity is O(1)
      */
     public boolean isEmpty() {
         return n == 0;
     }
-
-    /**
-     * Is {@code i} an index on this priority queue?
-     *
-     * @param  i an index
-     * @return {@code true} if {@code i} is an index on this priority queue;
-     *         {@code false} otherwise
-     * @throws IllegalArgumentException unless {@code 0 <= i < maxN}
-     */
-    public boolean contains(int i) {
-        if (i < 0 || i >= maxN) throw new IllegalArgumentException();
-        return qp[i] != -1;
-    }
-
     /**
      * Returns the number of keys on this priority queue.
-     *
      * @return the number of keys on this priority queue
+     * time complexity is O(1)
      */
     public int size() {
         return n;
     }
-
     /**
-     * Associates key with index {@code i}.
-     *
-     * @param  i an index
-     * @param  key the key to associate with index {@code i}
-     * @throws IllegalArgumentException unless {@code 0 <= i < maxN}
-     * @throws IllegalArgumentException if there already is an item associated
-     *         with index {@code i}
+     * Returns a smallest key on this priority queue.
+     * @return a smallest key on this priority queue.
+     * time complexity is O(1)
      */
-    public void insert(int i, Key key) {
-        if (i < 0 || i >= maxN) throw new IllegalArgumentException();
-        if (contains(i)) throw new IllegalArgumentException("index is already in the priority queue");
-        n++;
-        qp[i] = n;
-        pq[n] = i;
-        keys[i] = key;
-        swim(n);
-    }
-
-    /**
-     * Returns an index associated with a minimum key.
-     *
-     * @return an index associated with a minimum key
-     * @throws NoSuchElementException if this priority queue is empty
-     */
-    public int minIndex() {
-        if (n == 0) throw new NoSuchElementException("Priority queue underflow");
+    public Key min() {
+        if (isEmpty()) {
+            return null;
+        }
         return pq[1];
     }
-
     /**
-     * Returns a minimum key.
-     *
-     * @return a minimum key
-     * @throws NoSuchElementException if this priority queue is empty
+     * resize method to resize the array.
+     * @param      capacity  The capacity
      */
-    public Key minKey() {
-        if (n == 0) throw new NoSuchElementException("Priority queue underflow");
-        return keys[pq[1]];
+    private void resize(final int capacity) {
+        assert capacity > n;
+        Key[] temp = (Key[]) new Object[capacity];
+        for (int i = 1; i <= n; i++) {
+            temp[i] = pq[i];
+        }
+        pq = temp;
     }
-
     /**
-     * Removes a minimum key and returns its associated index.
-     * @return an index associated with a minimum key
-     * @throws NoSuchElementException if this priority queue is empty
+     * Adds a new key to this priority queue.
+     * @param  x the key to add to this priority queue
+     * time complexity is O(log(n))
      */
-    public int delMin() {
-        if (n == 0) throw new NoSuchElementException("Priority queue underflow");
-        int min = pq[1];
+    public void insert(final Key x) {
+        if (n == pq.length - 1) {
+            resize(2 * pq.length);
+        }
+        pq[++n] = x;
+        swim(n);
+    }
+    /**
+     * Removes and returns a smallest key on this priority queue.
+     * @return a smallest key on this priority queue
+     * time complexity is O(log(n))
+     */
+    public Key delMin() {
+        if (isEmpty()) {
+            return null;
+        }
+        Key min = pq[1];
         exch(1, n--);
         sink(1);
-        assert min == pq[n+1];
-        qp[min] = -1;        // delete
-        keys[min] = null;    // to help with garbage collection
-        pq[n+1] = -1;        // not needed
+        pq[n + 1] = null;
+        final int four = 4;
+        if ((n > 0) && (n == (pq.length - 1) / four)) {
+            resize(pq.length / 2);
+        }
         return min;
     }
-
     /**
-     * Returns the key associated with index {@code i}.
-     *
-     * @param  i the index of the key to return
-     * @return the key associated with index {@code i}
-     * @throws IllegalArgumentException unless {@code 0 <= i < maxN}
-     * @throws NoSuchElementException no key is associated with index {@code i}
+     * swim method.
+     * @param      k    index.
+     * time complexity is O(log(n))
      */
-    public Key keyOf(int i) {
-        if (i < 0 || i >= maxN) throw new IllegalArgumentException();
-        if (!contains(i)) throw new NoSuchElementException("index is not in the priority queue");
-        else return keys[i];
+    private void swim(final int k) {
+        int k1 = k;
+        while (k1 > 1 && greater(k1 / 2, k1)) {
+            exch(k1, k1 / 2);
+            k1 = k1 / 2;
+        }
     }
-
     /**
-     * Change the key associated with index {@code i} to the specified value.
-     *
-     * @param  i the index of the key to change
-     * @param  key change the key associated with index {@code i} to this key
-     * @throws IllegalArgumentException unless {@code 0 <= i < maxN}
-     * @throws NoSuchElementException no key is associated with index {@code i}
+     * sink method.
+     * @param      k    index.
+     * time complexity is O(log(n))
      */
-    public void changeKey(int i, Key key) {
-        if (i < 0 || i >= maxN) throw new IllegalArgumentException();
-        if (!contains(i)) throw new NoSuchElementException("index is not in the priority queue");
-        keys[i] = key;
-        swim(qp[i]);
-        sink(qp[i]);
+    private void sink(final int k) {
+        int k1 = k;
+        while (2 * k1 <= n) {
+            int j = 2 * k1;
+            if (j < n && greater(j, j + 1)) {
+                j++;
+            }
+            if (!greater(k1, j)) {
+                break;
+            }
+            exch(k1, j);
+            k1 = j;
+        }
     }
-
     /**
-     * Change the key associated with index {@code i} to the specified value.
-     *
-     * @param  i the index of the key to change
-     * @param  key change the key associated with index {@code i} to this key
-     * @throws IllegalArgumentException unless {@code 0 <= i < maxN}
-     * @deprecated Replaced by {@code changeKey(int, Key)}.
+     * greater method.
+     * @param      i     index.
+     * @param      j     index.
+     * @return     true or false.
+     * time complexity is O(1)
      */
-    @Deprecated
-    public void change(int i, Key key) {
-        changeKey(i, key);
+    private boolean greater(final int i, final int j) {
+        if (comparator == null) {
+            return ((Comparable<Key>) pq[i]).compareTo(pq[j]) > 0;
+        } else {
+            return comparator.compare(pq[i], pq[j]) > 0;
+        }
     }
-
     /**
-     * Decrease the key associated with index {@code i} to the specified value.
-     *
-     * @param  i the index of the key to decrease
-     * @param  key decrease the key associated with index {@code i} to this key
-     * @throws IllegalArgumentException unless {@code 0 <= i < maxN}
-     * @throws IllegalArgumentException if {@code key >= keyOf(i)}
-     * @throws NoSuchElementException no key is associated with index {@code i}
+     * exch method to swap the elements.
+     * @param      i     index.
+     * @param      j     index.
+     * time complexity is O(1)
      */
-    public void decreaseKey(int i, Key key) {
-        if (i < 0 || i >= maxN) throw new IllegalArgumentException();
-        if (!contains(i)) throw new NoSuchElementException("index is not in the priority queue");
-        if (keys[i].compareTo(key) <= 0)
-            throw new IllegalArgumentException("Calling decreaseKey() with given argument would not strictly decrease the key");
-        keys[i] = key;
-        swim(qp[i]);
-    }
-
-    /**
-     * Increase the key associated with index {@code i} to the specified value.
-     *
-     * @param  i the index of the key to increase
-     * @param  key increase the key associated with index {@code i} to this key
-     * @throws IllegalArgumentException unless {@code 0 <= i < maxN}
-     * @throws IllegalArgumentException if {@code key <= keyOf(i)}
-     * @throws NoSuchElementException no key is associated with index {@code i}
-     */
-    public void increaseKey(int i, Key key) {
-        if (i < 0 || i >= maxN) throw new IllegalArgumentException();
-        if (!contains(i)) throw new NoSuchElementException("index is not in the priority queue");
-        if (keys[i].compareTo(key) >= 0)
-            throw new IllegalArgumentException("Calling increaseKey() with given argument would not strictly increase the key");
-        keys[i] = key;
-        sink(qp[i]);
-    }
-
-    /**
-     * Remove the key associated with index {@code i}.
-     *
-     * @param  i the index of the key to remove
-     * @throws IllegalArgumentException unless {@code 0 <= i < maxN}
-     * @throws NoSuchElementException no key is associated with index {@code i}
-     */
-    public void delete(int i) {
-        if (i < 0 || i >= maxN) throw new IllegalArgumentException();
-        if (!contains(i)) throw new NoSuchElementException("index is not in the priority queue");
-        int index = qp[i];
-        exch(index, n--);
-        swim(index);
-        sink(index);
-        keys[i] = null;
-        qp[i] = -1;
-    }
-
-
-   /***************************************************************************
-    * General helper functions.
-    ***************************************************************************/
-    private boolean greater(int i, int j) {
-        return keys[pq[i]].compareTo(keys[pq[j]]) > 0;
-    }
-
-    private void exch(int i, int j) {
-        int swap = pq[i];
+    private void exch(final int i, final int j) {
+        Key swap = pq[i];
         pq[i] = pq[j];
         pq[j] = swap;
-        qp[pq[i]] = i;
-        qp[pq[j]] = j;
     }
-
-
-   /***************************************************************************
-    * Heap helper functions.
-    ***************************************************************************/
-    private void swim(int k) {
-        while (k > 1 && greater(k/2, k)) {
-            exch(k, k/2);
-            k = k/2;
-        }
-    }
-
-    private void sink(int k) {
-        while (2*k <= n) {
-            int j = 2*k;
-            if (j < n && greater(j, j+1)) j++;
-            if (!greater(k, j)) break;
-            exch(k, j);
-            k = j;
-        }
-    }
-
-
-   /***************************************************************************
-    * Iterators.
-    ***************************************************************************/
-
     /**
-     * Returns an iterator that iterates over the keys on the
-     * priority queue in ascending order.
-     * The iterator doesn't implement {@code remove()} since it's optional.
-     *
-     * @return an iterator that iterates over the keys in ascending order
+     * Determines if minimum heap.
+     * @return     True if minimum heap, False otherwise.
+     * time complexity is O(1)
      */
-    public Iterator<Integer> iterator() { return new HeapIterator(); }
-
-    private class HeapIterator implements Iterator<Integer> {
-        // create a new pq
-        private IndexMinPQ<Key> copy;
-
-        // add all elements to copy of heap
-        // takes linear time since already in heap order so no keys move
-        public HeapIterator() {
-            copy = new IndexMinPQ<Key>(pq.length - 1);
-            for (int i = 1; i <= n; i++)
-                copy.insert(pq[i], keys[pq[i]]);
+    private boolean isMinHeap() {
+        return isMinHeap(1);
+    }
+    /**
+     * Determines if minimum heap.
+     * @param      k     index.
+     * @return     True if minimum heap, False otherwise.
+     * time complexity is O(1)
+     */
+    private boolean isMinHeap(final int k) {
+        if (k > n) {
+            return true;
         }
-
-        public boolean hasNext()  { return !copy.isEmpty();                     }
-        public void remove()      { throw new UnsupportedOperationException();  }
-
-        public Integer next() {
-            if (!hasNext()) throw new NoSuchElementException();
-            return copy.delMin();
+        int left = 2 * k;
+        int right = 2 * k + 1;
+        if (left  <= n && greater(k, left)) {
+            return false;
         }
+        if (right <= n && greater(k, right)) {
+            return false;
+        }
+        return isMinHeap(left) && isMinHeap(right);
     }
 }
