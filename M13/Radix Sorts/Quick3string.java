5cd60b127275248185626d9eee1034c69213092e
Meher143 <krishmeher@msitprogram.net>
2018-11-12 22:57:38 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- M13/Radix Sorts/Quick3string.java
+++ M13/Radix Sorts/Quick3string.java
@@ -1,129 +1,131 @@
-/******************************************************************************
- *  Compilation:  javac Quick3string.java
- *  Execution:    java Quick3string < input.txt
- *  Dependencies: StdIn.java StdOut.java 
- *  Data files:   https://algs4.cs.princeton.edu/51radix/words3.txt
- *                https://algs4.cs.princeton.edu/51radix/shells.txt
- *
- *  Reads string from standard input and 3-way string quicksort them.
- *
- *  % java Quick3string < shell.txt
- *  are
- *  by
- *  sea
- *  seashells
- *  seashells
- *  sells
- *  sells
- *  she
- *  she
- *  shells
- *  shore
- *  surely
- *  the
- *  the
- *
- *
- ******************************************************************************/
-
-import java.util.Random;
-
 /**
- *  The {@code Quick3string} class provides static methods for sorting an
- *  array of strings using 3-way radix quicksort.
- *  <p>
- *  For additional documentation,
- *  see <a href="https://algs4.cs.princeton.edu/51radix">Section 5.1</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
- *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
+ * Class for quick 3 string.
  */
 public class Quick3string {
-    private static final int CUTOFF =  15;   // cutoff to insertion sort
-
-    // do not instantiate
-    Quick3string() { } 
-
-    /**  
-     * Rearranges the array of strings in ascending order.
-     *
-     * @param a the array to be sorted
+    /**
+     * cutoff to insertion sort.
      */
-    public static void sort(String[] a) {
-        StdRandom.shuffle(a);
-        sort(a, 0, a.length-1, 0);
+    private static final int CUTOFF =  15;
+    /**
+     * Rearranges the array of strings in ascending order.
+     * @param a the array to be sorted
+     * Time complexity for this method is O(W*N*log N).
+     */
+    public void sort(final String[] a) {
+        sort(a, 0, a.length - 1, 0);
         assert isSorted(a);
     }
-
-    // return the dth character of s, -1 if d = length of s
-    private static int charAt(String s, int d) { 
+    /**
+     * return the dth character of s, -1 if d = length of s.
+     * @param      s     String.
+     * @param      d     int.
+     * @return     int.
+     * Time complexity for this method is O(1).
+     */
+    private int charAt(final String s, final int d) {
         assert d >= 0 && d <= s.length();
-        if (d == s.length()) return -1;
+        if (d == s.length()) {
+            return -1;
+        }
         return s.charAt(d);
     }
-
-
-    // 3-way string quicksort a[lo..hi] starting at dth character
-    private static void sort(String[] a, int lo, int hi, int d) { 
-
+    /**
+     * 3-way string quicksort a[lo..hi] starting at dth character.
+     * @param      a     String array.
+     * @param      lo    The lower
+     * @param      hi    The higher
+     * @param      d     index.
+     * Time complexity for this method is O(W*N*log N).
+     */
+    private void sort(final String[] a, final int lo,
+        final int hi, final int d) {
         // cutoff to insertion sort for small subarrays
         if (hi <= lo + CUTOFF) {
             insertion(a, lo, hi, d);
             return;
         }
-
         int lt = lo, gt = hi;
         int v = charAt(a[lo], d);
         int i = lo + 1;
         while (i <= gt) {
             int t = charAt(a[i], d);
-            if      (t < v) exch(a, lt++, i++);
-            else if (t > v) exch(a, i, gt--);
-            else              i++;
+            if (t < v) {
+                exch(a, lt++, i++);
+            } else if (t > v) {
+                exch(a, i, gt--);
+            } else {
+                i++;
+            }
         }
-
-        // a[lo..lt-1] < v = a[lt..gt] < a[gt+1..hi]. 
-        sort(a, lo, lt-1, d);
-        if (v >= 0) sort(a, lt, gt, d+1);
-        sort(a, gt+1, hi, d);
+        // a[lo..lt-1] < v = a[lt..gt] < a[gt+1..hi].
+        sort(a, lo, lt - 1, d);
+        if (v >= 0) {
+            sort(a, lt, gt, d + 1);
+        }
+        sort(a, gt + 1, hi, d);
     }
-
-    // sort from a[lo] to a[hi], starting at the dth character
-    private static void insertion(String[] a, int lo, int hi, int d) {
-        for (int i = lo; i <= hi; i++)
-            for (int j = i; j > lo && less(a[j], a[j-1], d); j--)
-                exch(a, j, j-1);
+    /**
+     * sort from a[lo] to a[hi], starting at the dth character.
+     * @param      a     String array.
+     * @param      lo    The lower
+     * @param      hi    The higher
+     * @param      d     index.
+     * Time complexity for this method is O(N^2).
+     */
+    private void insertion(final String[] a, final int lo,
+        final int hi, final int d) {
+        for (int i = lo; i <= hi; i++) {
+            for (int j = i; j > lo && less(a[j], a[j - 1], d); j--) {
+                exch(a, j, j - 1);
+            }
+        }
     }
-
-    // exchange a[i] and a[j]
-    private static void exch(String[] a, int i, int j) {
+    /**
+     * exchange a[i] and a[j].
+     * @param      a     String array.
+     * @param      i     index.
+     * @param      j     index.
+     * Time complexity for this method is O(1).
+     */
+    private void exch(final String[] a, final int i,
+        final int j) {
         String temp = a[i];
         a[i] = a[j];
         a[j] = temp;
     }
-
-    // is v less than w, starting at character d
-    // DEPRECATED BECAUSE OF SLOW SUBSTRING EXTRACTION IN JAVA 7
-    // private static boolean less(String v, String w, int d) {
-    //    assert v.substring(0, d).equals(w.substring(0, d));
-    //    return v.substring(d).compareTo(w.substring(d)) < 0; 
-    // }
-
-    // is v less than w, starting at character d
-    private static boolean less(String v, String w, int d) {
+    /**
+     * less method.
+     * @param      v     String.
+     * @param      w     String.
+     * @param      d     index.
+     * @return     true or false.
+     * Time complexity for this method is O(v) or O(w).
+     */
+    private boolean less(final String v, final String w,
+        final int d) {
         assert v.substring(0, d).equals(w.substring(0, d));
         for (int i = d; i < Math.min(v.length(), w.length()); i++) {
-            if (v.charAt(i) < w.charAt(i)) return true;
-            if (v.charAt(i) > w.charAt(i)) return false;
+            if (v.charAt(i) < w.charAt(i)) {
+                return true;
+            }
+            if (v.charAt(i) > w.charAt(i)) {
+                return false;
+            }
         }
         return v.length() < w.length();
     }
-
-    // is the array sorted
-    private static boolean isSorted(String[] a) {
-        for (int i = 1; i < a.length; i++)
-            if (a[i].compareTo(a[i-1]) < 0) return false;
+    /**
+     * Determines if sorted.
+     * @param      a     String array.
+     * @return     True if sorted, False otherwise.
+     * Time complexity for this method is O(a).
+     */
+    private boolean isSorted(final String[] a) {
+        for (int i = 1; i < a.length; i++) {
+            if (a[i].compareTo(a[i - 1]) < 0) {
+                return false;
+            }
+        }
         return true;
     }
 }
--- M13/Radix Sorts/Solution.java
+++ M13/Radix Sorts/Solution.java
@@ -1,21 +1,28 @@
-import java.util.*;
-class Solution {
-	Solution() {
+import java.util.Scanner;
+import java.util.Arrays;
+/**
+ * Class for solution.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
     }
     /**
-     * main function.
-     *
+     * main method that drives the program.
      * @param      args  The arguments
+     * Time complexity for this method is O(W*N*log N).
      */
     public static void main(final String[] args) {
-    	Scanner sc = new Scanner(System.in);
-    	int n = Integer.parseInt(sc.nextLine());
-    	String[] str = new String[n];
-    	Quick3string qr = new Quick3string();
-    	for (int i = 0; i < n; i++) {
-    		str[i] = sc.nextLine();
+        Scanner sc = new Scanner(System.in);
+        Quick3string q = new Quick3string();
+        int n = Integer.parseInt(sc.nextLine());
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextLine();
         }
-        qr.sort(str);
-        System.out.println(Arrays.toString(str));
+        q.sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
