bcdce6bbe5964960ec149b55b678dae585206b3f
Meher143 <krishmeher@msitprogram.net>
2018-11-17 12:31:40 +0530

    "ads2_m18_a1_1: 2/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- M18/ADS-2Exam-3/Solution.java
+++ M18/ADS-2Exam-3/Solution.java
@@ -120,6 +120,9 @@ public class Solution {
     }
 
 }
+/**
+ * Class for t 9.
+ */
 
 class T9 {
     private TST dict;
@@ -134,26 +137,52 @@ class T9 {
         }
     }
 
-    // get all the prefixes that match with given prefix.
+    /**
+     * Gets all words.
+     *
+     * @param      prefix  The prefix.
+     *
+     * @return     All words.
+     */
     public Iterable<String> getAllWords(String prefix) {
         // your code goes here
         return dict.keysWithPrefix(prefix);
     }
+    /**
+     * { function_description }.
+     *
+     * @param      t9Signature  The t 9 signature.
+     *
+     * @return     { description_of_the_return_value }.
+     */
 
     public Iterable<String> potentialWords(String t9Signature) {
         // your code goes here
         return null;
     }
 
-    // return all possibilities(words), find top k with highest frequency.
-    public Iterable<String> getSuggestions(Iterable<String> words, int k) {
+    /**
+     * Gets the suggestions.
+     *
+     * @param      words  The words.
+     * @param      k      { parameter_description }.
+     *
+     * @return     The suggestions.
+     */
+    public Iterable<String> getSuggestions(Iterable<String> words, final int k) {
         // your code goes here
         return null;
     }
 
-    // final output
-    // Don't modify this method.
-    public Iterable<String> t9(String t9Signature, int k) {
+    /**
+     * { function_description }.
+     *
+     * @param      t9Signature  The t 9 signature.
+     * @param      k            { parameter_description }.
+     *
+     * @return     { description_of_the_return_value }.
+     */
+    public Iterable<String> t9(String t9Signature, final int k) {
         return getSuggestions(potentialWords(t9Signature), k);
     }
 }
